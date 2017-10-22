9c249628db34f25f22447a02e1c0be6ed0c7ca8f
lawrencekhlim <lawrencekhlim@gmail.com>
2017-10-22 00:02:59 -0700

    Factored out the sound player.

--- rails.py
+++ rails.py
@@ -1,12 +1,12 @@
 import tkinter
 import time
-import pygame as pg
 import numpy as np
 import librosa
 import os
 import csv
 
 import soundanalysis
+import mediaplayer
 
 class SimpleLayoutApplication:
 
@@ -150,7 +150,7 @@ class SimpleLayoutApplication:
         self.sounds.pause()
     
     def run(self):
-        self.sounds = Sound ()
+        self.sounds = mediaplayer.AudioPlayer()
         self.sounds.play_music (self.filename)
         
         #play_music(self.filename)
@@ -239,46 +239,6 @@ class SimpleLayoutApplication:
         diff = larger-smaller
         return smaller + diff * percent
 
-
-class Sound:
-    def __init__(self):
-       self.isPaused = False
-    
-    def play_music(self, music_file, volume=0.5):
-        '''
-            stream music with mixer.music module in a blocking manner
-            this will stream the sound from disk while playing
-        '''
-        
-        # set up the mixer
-        freq = 44100     # audio CD quality
-        bitsize = -16    # unsigned 16 bit
-        channels = 2     # 1 is mono, 2 is stereo
-        buffer = 2048    # number of samples (experiment to get best sound)
-        pg.mixer.init(freq, bitsize, channels, buffer)
-        # volume value 0.0 to 1.0
-        pg.mixer.music.set_volume(volume)
-        clock = pg.time.Clock()
-        try:
-            pg.mixer.music.load(music_file)
-            print("Music file {} loaded!".format(music_file))
-        except pg.error:
-            print("File {} not found! ({})".format(music_file, pg.get_error()))
-            return
-        pg.mixer.music.play(0)
-    def pause(self):
-        if not self.isPaused:
-            pg.mixer.music.pause()
-            self.isPaused = True
-        else:
-            pg.mixer.music.unpause()
-            self.isPaused = False
-    
-    def get_position (self):
-        return pg.mixer.music.get_pos()
-
-    def isBusy(self):
-        return pg.mixer.music.get_busy()
 
 class MovingRectangle:
     def __init__ (self, endtime, startingX, width, canvasheight, timetoReachBottom=1000):
